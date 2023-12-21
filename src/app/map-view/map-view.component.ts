16ee8e83194ef36a8a0e878d01e108ecc0f20113
Safia Ahmed <safia_ahmed52@hotmail.com>
2023-12-21 00:13:44 -0500

    v2

--- src/app/map-view/map-view.component.ts
+++
@@ -1,9 +0,0 @@
-import { AfterViewInit, Component, ElementRef, NgZone, ViewChild } from '@angular/core';
-
-@Component({
-  selector: 'app-map-view',
-  template: '<div #mapContainer style="height: 400px;"></div>',
-})
-export class MapViewComponent {
- 
-}
